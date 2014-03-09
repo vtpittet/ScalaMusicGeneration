410a647bc0c51e2c42ed0a3f5e49a8c2070296dd
Valerian Pittet <valerian.pittet@epfl.ch>
2014-03-09 18:25:40 +0100

    Pitch rewritten to allow # and b alterations

--- MusicInterface/src/tonalSystem/Pitch.scala
+++ MusicInterface/src/tonalSystem/Pitch.scala
@@ -1,130 +1,88 @@
 package tonalSystem
 
-sealed trait Pitch {
+sealed trait Pitch { self =>
   val octave: Int
+  val alter: Int
   
-  def +(halfTone: Int): Pitch
-  def -(halfTone: Int): Pitch
+  val next: Pitch
+  val prev: Pitch
+  
+  val newSelf: (Int, Int) => Pitch
+  
+  def +(steps: Int): Pitch = {
+    if (steps < 0) prev - (steps + 1)
+    else if (steps == 0) self
+    else prev + (steps - 1)
+  }
+  def -(steps: Int): Pitch = self + (-steps)
+  
+  def is(times: Int = 1) = newSelf(octave, alter+times)
+  def es(times: Int = 1) = is(-times)
 }
 
-
-case class A(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new AS(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new A(octave-1) + (12-halfTone)
-}
-
-case class AS(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new B(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new AS(octave-1) + (12-halfTone)
-}
-
-case class B(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new C(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new B(octave-1) + (12-halfTone)
-}
-
-case class C(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new CS(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new C(octave-1) + (12-halfTone)
-}
-case class CS(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new D(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new CS(octave-1) + (12-halfTone)
-}
-case class D(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new DS(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new D(octave-1) + (12-halfTone)
-}
-case class DS(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new E(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new DS(octave-1) + (12-halfTone)
-}
-case class E(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new F(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new E(octave-1) + (12-halfTone)
-}
-case class F(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new FS(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new F(octave-1) + (12-halfTone)
-}
-case class FS(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new G(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new FS(octave-1) + (12-halfTone)
-}
-case class G(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new GS(octave)+(halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new G(octave-1) + (12-halfTone)
-}
-case class GS(octave: Int) extends Pitch {
-  def +(halfTone: Int): Pitch = if (halfTone == 0) {
-    this
-  } else {
-    new A(octave + 1) + (halfTone-1)
-  }
-  def -(halfTone: Int): Pitch = new GS(octave-1) + (12-halfTone)
-}
-
-// Rest
 case object S extends Pitch {
   val octave = 0
-  def +(halfTone: Int) = S
-  def -(halfTone: Int) = S
+  val alter = 0
+  val prev = S
+  val next = S
+  val newSelf = (o: Int, a: Int) => S
+}
+
+case class A(octave: Int, alter: Int) extends Pitch {
+  lazy val next = B(octave, alter)
+  lazy val prev = G(octave - 1, alter)
+  
+  val newSelf = (o: Int, a: Int) => A(o, a)
+}
+
+case class B(octave: Int, alter: Int) extends Pitch {
+  lazy val next = C(octave, alter)
+  lazy val prev = A(octave, alter)
+  
+  val newSelf = (o: Int, a: Int) => B(o, a)
+}
+
+case class C(octave: Int, alter: Int) extends Pitch {
+  lazy val next = D(octave, alter)
+  lazy val prev = B(octave, alter)
+  
+  val newSelf = (o: Int, a: Int) => C(o, a)
+}
+
+case class D(octave: Int, alter: Int) extends Pitch {
+  lazy val next = E(octave, alter)
+  lazy val prev = C(octave, alter)
+  
+  val newSelf = (o: Int, a: Int) => D(o, a)
+}
+
+case class E(octave: Int, alter: Int) extends Pitch {
+  lazy val next = F(octave, alter)
+  lazy val prev = D(octave, alter)
+  
+  val newSelf = (o: Int, a: Int) => E(o, a)
+}
+
+case class F(octave: Int, alter: Int) extends Pitch {
+  lazy val next = G(octave, alter)
+  lazy val prev = E(octave, alter)
+  
+  val newSelf = (o: Int, a: Int) => F(o, a)
+}
+
+case class G(octave: Int, alter: Int) extends Pitch {
+  lazy val next = A(octave + 1, alter)
+  lazy val prev = F(octave, alter)
+  
+  val newSelf = (o: Int, a: Int) => G(o, a)
 }
 
 
-object A extends A(0)
-object AS extends AS(0)
-object B extends B(0)
-object C extends C(0)
-object CS extends CS(0)
-object D extends D(0)
-object DS extends DS(0)
-object E extends E(0)
-object F extends F(0)
-object FS extends FS(0)
-object G extends G(0)
-object GS extends GS(0)
+
+object A extends A(0, 0)
+object B extends B(0, 0)
+object C extends C(0, 0)
+object D extends D(0, 0)
+object E extends E(0, 0)
+object F extends F(0, 0)
+object G extends G(0, 0)
