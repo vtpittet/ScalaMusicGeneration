0fa501f50e7613ef4afd30d23d9c9ea7d42b3bc4
Valerian Pittet <valerian.pittet@epfl.ch>
2014-06-02 00:20:36 +0200

    Case study representation started as sentence types, aiming rel generation

--- MusicInterface/test/caseStudy/NewRecuerdosP1.scala
+++ MusicInterface/test/caseStudy/NewRecuerdosP1.scala
@@ -15,10 +15,56 @@ import tonalSystem.A
 import segmentSystem.ClassPredicate.isSeq
 import segmentSystem.Sequential
 import tonalSystem.Tone._
+import tonalSystem.Tone
 import segmentSystem.ClassPredicate
 import segmentSystem.Note
 
-object RecuerdosP1 extends App with MelodyWriter {
+object NewRecuerdosP1 extends App with MelodyWriter {
+  
+  
+  
+  def makeMesureT1(s1: Tone, s2: Tone, b1: Tone, b2: Tone, ts: Int, tb: Int): MS = {
+    
+    val fs1 = {
+      implicit val noteDuration = rT
+      s1 *4 + (s1 + ts) *2 mapNotes { O + _ *3 }
+    }
+    val fs2b1 = {
+      implicit val noteDuration = rE
+      O + b1 + s2 + b1 + (s2 + ts) + (b1 + tb)
+    }
+    
+    val fb2 = b2(rH-)
+    
+    fs1 | fs2b1 | fb2
+  }
+  
+  def phraseT1(tonic: Tone, ret: Boolean, ts: Int, tb: Int): MS = {
+    val b2 = tonic
+    val b1 = tonic increaseBy 4
+    val s12 = tonic increaseBy (7 + 2)
+    val s11 = tonic increaseBy (7 + 4)
+    val ts1 = if (ret) -1 else 0
+    val tb1 = 0
+    
+    val s22 = if (ret) tonic increaseBy 7 else s12
+    val s21 = if (ret) tonic increaseBy (7 + 2) else s11
+    
+    val m1 = makeMesureT1(s11, s12, b1, b2, ts1, tb1)
+    
+    val m2 = makeMesureT1(s21, s22, b1, b2, ts, tb)
+    
+    m1 + m2
+  }
+  
+  
+  val aaCCF = {
+    phraseT1(I(-1), true, 1, 0) +
+    phraseT1(I(-1), false, 1, 2) +
+    phraseT1(III(-1), true, 1, 0) +
+    phraseT1(III(-1), false, 0, 0) +
+    phraseT1(VI(-1), true, 1, 1)
+  }
   
   case class StdSop(melody: List[MusicalSegment]) extends SequentialSegment {
     override val buildFromMelody = StdSop(_)
@@ -212,7 +258,7 @@ object RecuerdosP1 extends App with MelodyWriter {
   // Short version without repetitions
   MelodyPlayer(
     Sequential(Nil)
-    + (part1 withScale minScale)
+    + (aaCCF withScale minScale)
     ,
     tempo,
 //    fromQN = 14*3,
