58ac8379f06cc00ef7c8a5e9340f523dc2f636cb
Maelle Colussi <maellecolussi@yahoo.fr>
2014-10-11 14:34:36 +0200

    more in harmonize

--- MMusGenScala/src/gen/HarmonyGen.scala
+++ MMusGenScala/src/gen/HarmonyGen.scala
@@ -3,57 +3,96 @@ package gen
 import segmentSystem._
 import tonalSystem.Tone
 import tonalSystem.Tone._
+import scala.util.Random
 
-case class HarmonyGen(melody: SimpleMelody, upperBound: Note, lowerBound: Note) {
+case class HarmonyGen(sMelody: SimpleMelody/*, lowerBound: Note*/, allChords : List[CharChord]) {
   
-
-}
-
-//TODO : perhaps bad structure
-/* char is the characteristic tone of the degree : from 1 to 7*/
-class Degree(char : Int) {
-  def +(degRise: Int) : Degree = {
-    return new Degree(((char - 1 + degRise)%7 + 7)%7 + 1);
-  }
-  
-}
-
-object Utilities {
-  def toneToDegree(t : Tone) : Degree = {
+  def harmonize : MusicalSegment = {
+    //test if lowerBound consistent (not higher than the melody) at least
+    
+    //for now, put lower bound is 2 octaves below the first note of the melody
+    // later : 2 octaves below the lowest note of the melody
+    val lowerBound = ???
+    
+    val possibleChords : List[List[CharChord]] = sMelody.melody.map(x => getPossChords(Ut.toneToDegree(x.tone)))
+    
+    
+    //for now : take randomly one chord for each note
+    val chosenChords = possibleChords.map(x => chooseOneIn(x))
+    
+    
     ???
   }
+  def getPossChords(d : Degree) : List[CharChord] = {
+    allChords.filter(x  => x.belongsToChord(d))
+  }
+  
+  def chooseOneIn[A](l : List[A]) : A = {
+    Random.shuffle(l).head //TODO does this change the original ? (I think not but have a doubt)
+  }
+
 }
 
-//TODO : create Degree for type of degree that is Tone modulo 8
+//TODO : perhaps bad structure : is type of Tone without octaves 
+//TODO how to force char of degree being between 0 to 7 ?
+//TODO perhaps simply use one tone to characterize it ?
+// char is the characteristic tone of the degree : from 1 to 7 normally
+//can be zero when unknown
+case class Degree(char: Int, alt : Option[Boolean]) {
+  def +(degRise: Int): Degree = {
+    return new Degree(Ut.posMod(char - 1 + degRise, 7) + 1, alt);
+  }
+}
+
+object Ut {
+ def toneToDegree(t: Tone): Degree = {
+    t match {
+      case O => Degree(0, None)
+      case I(_, alt) => Degree(1, alt)
+      case II(_, alt) => Degree(2, alt)
+      case III(_,alt) => Degree(3, alt)
+      case IV(_, alt) => Degree(4, alt)
+      case V(_, alt) => Degree(5, alt)
+      case VI(_, alt) => Degree(6, alt)
+      case VII(_, alt) => Degree(7, alt)
+    }
+  }
+  def degreeToTone(d : Degree, octave: Int) : Tone = {
+    val i = posMod(d.char - 1, 7) + 1
+    if (i == 0) O
+    else if (i == 1) I(octave, d.alt)
+    else if (i == 2) II(octave, d.alt)
+    else if (i == 3) III(octave, d.alt)
+    else if (i == 4) IV(octave, d.alt)
+    else if (i == 5) V(octave, d.alt)
+    else if (i == 6) VI(octave, d.alt)
+    else VII(octave, d.alt)
+  }
+  
+  def posMod(i : Int, mod : Int) : Int = {
+    return (i % mod + mod) % mod
+  }
+}
 
 //chord with list of notes characterized by a degree, and perhaps with other things that will be added
 // (for triads and simple seventh it is ok like that)
-
-
 trait CharChord {
-  val tones : List[Degree] // first is the bass of the chord
-  def allowedRemain(setN : List[Degree]) : List[List[Degree]]
-}
-
-case class Triad(degree : Degree) extends CharChord{ 
-	//allowed sets of I, II, ... (Pitch ?)
-  def allowedRemain(setN : List[Degree]) : List[List[Degree]] = {
-    ???
+  val degrees: List[Degree] // first is the bass of the chord
+  def belongsToChord(d : Degree) : Boolean = {
+    degrees.contains(d)//TODO : force Degree be 0 to 7 only, or here use modulo
   }
-  val tones = degree :: (degree + 2) :: (degree + 4) :: Nil
-  	
 }
 
-case class SeventhCh(degree : Degree) extends CharChord{
-	//allowed sets of I, II, ... (Pitch ?)
-  def allowedRemain(setN : List[Degree]) : List[List[Degree]] = {
-    ???
-  }
-  val tones = degree :: (degree + 2) :: (degree + 4) :: (degree + 6) :: Nil
+case class Triad(degree: Degree) extends CharChord {
+  val degrees = degree :: (degree + 2) :: (degree + 4) :: Nil
 }
 
-case class Chord4(notes : List[Tone], template : CharChord){
-  
+case class SeventhCh(degree: Degree) extends CharChord {
+  val degrees = degree :: (degree + 2) :: (degree + 4) :: (degree + 6) :: Nil
+}
+
+case class Chord4(notes: List[Tone], template: CharChord) {
+
 }
 
 
