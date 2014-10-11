e5326dcdbadadb32f7bda8be02fff815e392ad75
Maelle Colussi <maellecolussi@yahoo.fr>
2014-10-11 16:41:16 +0200

    some details plus format in gen

--- MMusGenScala/src/gen/HarmonyGen.scala
+++ MMusGenScala/src/gen/HarmonyGen.scala
@@ -5,29 +5,26 @@ import tonalSystem.Tone
 import tonalSystem.Tone._
 import scala.util.Random
 
-case class HarmonyGen(sMelody: SimpleMelody/*, lowerBound: Note*/, allChords : List[CharChord]) {
-  
-  def harmonize : MusicalSegment = {
+case class HarmonyGen(sMelody: SimpleMelody /*, lowerBound: Note*/ , allChords: List[CharChord]) {
+
+  def harmonize: MusicalSegment = {
     //test if lowerBound consistent (not higher than the melody) at least
-    
-    //for now, put lower bound is 2 octaves below the first note of the melody
-    // later : 2 octaves below the lowest note of the melody
-    val lowerBound = ???
-    
-    val possibleChords : List[List[CharChord]] = sMelody.melody.map(x => getPossChords(Ut.toneToDegree(x.tone)))
-    
-    
-    //for now : take randomly one chord for each note
+
+    //TODO for now, 2 octaves below the lowest note of the melody 
+    val lowerBound = sMelody.melody.min(NoteOrdering) - 14
+
+    val possibleChords: List[List[CharChord]] = sMelody.melody.map(x => getPossChords(Ut.toneToDegree(x.tone)))
+
+    //TODO for now : take randomly one chord for each note
     val chosenChords = possibleChords.map(x => chooseOneIn(x))
-    
-    
+
     ???
   }
-  def getPossChords(d : Degree) : List[CharChord] = {
-    allChords.filter(x  => x.belongsToChord(d))
+  def getPossChords(d: Degree): List[CharChord] = {
+    allChords.filter(x => x.belongsToChord(d))
   }
-  
-  def chooseOneIn[A](l : List[A]) : A = {
+
+  def chooseOneIn[A](l: List[A]): A = {
     Random.shuffle(l).head //TODO does this change the original ? (I think not but have a doubt)
   }
 
@@ -38,26 +35,26 @@ case class HarmonyGen(sMelody: SimpleMelody/*, lowerBound: Note*/, allChords : L
 //TODO perhaps simply use one tone to characterize it ?
 // char is the characteristic tone of the degree : from 1 to 7 normally
 //can be zero when unknown
-case class Degree(char: Int, alt : Option[Boolean]) {
+case class Degree(char: Int, alt: Option[Boolean]) {
   def +(degRise: Int): Degree = {
     return new Degree(Ut.posMod(char - 1 + degRise, 7) + 1, alt);
   }
 }
 
 object Ut {
- def toneToDegree(t: Tone): Degree = {
+  def toneToDegree(t: Tone): Degree = {
     t match {
       case O => Degree(0, None)
       case I(_, alt) => Degree(1, alt)
       case II(_, alt) => Degree(2, alt)
-      case III(_,alt) => Degree(3, alt)
+      case III(_, alt) => Degree(3, alt)
       case IV(_, alt) => Degree(4, alt)
       case V(_, alt) => Degree(5, alt)
       case VI(_, alt) => Degree(6, alt)
       case VII(_, alt) => Degree(7, alt)
     }
   }
-  def degreeToTone(d : Degree, octave: Int) : Tone = {
+  def degreeToTone(d: Degree, octave: Int): Tone = {
     val i = posMod(d.char - 1, 7) + 1
     if (i == 0) O
     else if (i == 1) I(octave, d.alt)
@@ -68,9 +65,23 @@ object Ut {
     else if (i == 6) VI(octave, d.alt)
     else VII(octave, d.alt)
   }
-  
-  def posMod(i : Int, mod : Int) : Int = {
+
+  def posMod(i: Int, mod: Int): Int = {
     return (i % mod + mod) % mod
+  }
+
+  def toneIsLTOrE(t1: Tone, t2: Tone): Boolean = {
+    if (t1.octave != t2.octave) (t1.octave <= t2.octave)
+    else (toneToDegree(t1).char <= toneToDegree(t2).char)
+  }
+}
+
+object NoteOrdering extends Ordering[Note] {
+  def compare(a: Note, b: Note) = {
+    if (a.tone.octave != b.tone.octave) { if (a.tone.octave < b.tone.octave) -1 else 1 }
+    else if (Ut.toneToDegree(a.tone).char < Ut.toneToDegree(b.tone).char) -1
+    else if (Ut.toneToDegree(a.tone).char > Ut.toneToDegree(b.tone).char) 1
+    else 0
   }
 }
 
@@ -78,8 +89,8 @@ object Ut {
 // (for triads and simple seventh it is ok like that)
 trait CharChord {
   val degrees: List[Degree] // first is the bass of the chord
-  def belongsToChord(d : Degree) : Boolean = {
-    degrees.contains(d)//TODO : force Degree be 0 to 7 only, or here use modulo
+  def belongsToChord(d: Degree): Boolean = {
+    degrees.contains(d) //TODO : force Degree be 0 to 7 only, or here use modulo
   }
 }
 
