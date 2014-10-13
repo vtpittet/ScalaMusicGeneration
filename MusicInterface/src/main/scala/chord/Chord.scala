b6126a016371697d021671736f1b0c9f1b0b41bb
Valerian Pittet <valerian.pittet@epfl.ch>
2014-10-13 15:09:17 +0200

    chord classes added

---
+++ MusicInterface/src/main/scala/chord/Chord.scala
@@ -0,0 +1,26 @@
+import tonalSystem.Tone
+import tonalSystem.Tone.II
+
+sealed trait Chord {
+  val fun: Tone
+  val tones: List[Tone => Tone]
+
+  def apply(n: Int): Tone = {
+    def posMod(i: Int, mod: Int): Int = (i % mod + mod) % mod
+    tones(posMod(n, tones.size))(fun) increaseBy(n/tones.size *7) 
+  }
+}
+
+
+case class Triad(val fun: Tone) extends Chord {
+  val tones: List[Tone => Tone] = List(_ increaseBy 0, _ increaseBy 2, _ increaseBy 4)
+}
+
+case class Seventh(val fun: Tone) extends Chord {
+  val tones: List[Tone => Tone] = List(_ increaseBy 0, _ increaseBy 2, _ increaseBy 4, _ increaseBy 6)
+}
+
+case object NapSixth extends Chord {
+  val fun = II
+  val tones: List[Tone => Tone] = List(_ es, _ increaseBy 2, _ increaseBy 4)
+}
