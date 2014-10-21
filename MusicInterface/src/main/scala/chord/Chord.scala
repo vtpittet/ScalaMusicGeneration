47f796bd16358ef5727f5d2e6b58977a62670e8d
Maelle Colussi <maellecolussi@yahoo.fr>
2014-10-21 17:44:43 +0200

    add secondary dominants

--- MusicInterface/src/main/scala/chord/Chord.scala
+++ MusicInterface/src/main/scala/chord/Chord.scala
@@ -1,7 +1,7 @@
 package chord
 
 import tonalSystem.Tone
-import tonalSystem.Tone.II
+import tonalSystem.Tone._
 
 sealed trait Chord {
   val fun: Tone
@@ -28,4 +28,41 @@ case class Seventh(val fun: Tone) extends Chord {
 case object NapSixth extends Chord {
   val fun = II
   val tones: List[Tone => Tone] = List(_ es, _ increaseBy 2, _ increaseBy 4)
+}
+
+//MC : not sure yet
+//for minor harmonic
+case class SecDomMinH(fun: Tone) extends Chord {
+  //exists in minor : V of III, IV, V, VI
+  val tones: List[Tone => Tone] = fun match {
+    case III(_, None) => List(_.decreaseBy(3).is, _ decreaseBy 1, _ increaseBy 1, _ increaseBy 3)
+    case IV(_, None) => List(_ decreaseBy 3, _.decreaseBy(1).is, _ increaseBy 1, _.increaseBy(3).es)
+    case  V(_, None) => List(_ decreaseBy 3, _ decreaseBy 1, _.increaseBy(1).es, _.increaseBy(3).es)
+    case VI(_, None) => List(_ decreaseBy 3, _.decreaseBy(1).is, _.increaseBy(1).is, _ increaseBy 3)
+    case _ => Triad(fun decreaseBy 3).tones
+  }
+}
+
+//for minor natural
+case class SecDomMinN(fun: Tone) extends Chord {
+  //exists in minor : V of III, IV, V, VI, add VII
+  val tones: List[Tone => Tone] = fun match {
+    case III(_, None) => List(_ decreaseBy 3, _ decreaseBy 1, _ increaseBy 1, _ increaseBy 3)
+    case IV(_, None) | VII(_, None) =>
+      List(_ decreaseBy 3, _.decreaseBy(1).is, _ increaseBy 1, _ increaseBy 3)
+    case  V(_, None) => List(_ decreaseBy 3, _ decreaseBy 1, _ increaseBy 1, _.increaseBy(3).es)
+    case VI(_, None) => List(_ decreaseBy 3, _.decreaseBy(1).is, _.increaseBy(1).is, _ increaseBy 3)
+    case _ => Triad(fun decreaseBy 3).tones
+  }
+}
+
+//for major
+case class SecDomMaj(fun: Tone) extends Chord {
+  //exists in major : V of II, III, IV, V, VI
+  val tones: List[Tone => Tone] = fun match {
+    case II(_, None) | V(_, None) | VI(_, None) => List(_ decreaseBy 3, _.decreaseBy(1).is, _ increaseBy 1, _ increaseBy 3)
+    case III(_, None) => List(_ decreaseBy 3, _.decreaseBy(1).is, _.increaseBy(1).is, _ increaseBy 3)
+    case IV(_, None) => List(_ decreaseBy 3, _ decreaseBy 1, _ increaseBy 1, _.increaseBy(3).es)
+    case _ => Triad(fun decreaseBy 3).tones
+  }
 }
