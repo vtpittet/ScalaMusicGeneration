eaf5324515bb46f8a4c2d65173d3b9b08d94655a
Valerian Pittet <valerian.pittet@epfl.ch>
2014-12-16 17:43:24 +0100

    Base structure for grammar manipulation

---
+++ MusicInterface/src/main/scala/grammar/GrammarElement.scala
@@ -0,0 +1,35 @@
+package grammar
+
+// TODO specify composition rules
+// How to allow regex notation inside bodies ? (not compulsory)
+
+
+/** common top trait for grammar interface
+  */
+sealed trait GrammarElement[A]
+
+/** trait used to regroup word and epilon as a common
+  * case when generating sentences
+  */
+sealed trait Terminal[A] extends GrammarElement[A]
+
+/** Word represents the non-epsilon terminal values of the grammar
+  * Usually called Character of an alphabet, it would be confusing
+  * with the class Char
+  */
+case class Word[A](value: A) extends Terminal[A]
+
+/** Epsilon terminal of grammar
+  */
+case object Epsilon extends Terminal[Nothing]
+
+/** A Rule is a sequential list of grammar elements
+  */
+case class Rule[A](body: List[GrammarElement[A]]) extends GrammarElement[A]
+
+/** A production is a list of possible weighted rules. one has to be chosen
+  * in order to generate with the grammar
+  */
+case class Production[A](rules: List[(Rule[A], Double)]) extends GrammarElement[A]
+
+
