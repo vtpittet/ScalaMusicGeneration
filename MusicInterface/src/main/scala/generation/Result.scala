cc5a8c82e3c5e074901b5ded4cbeb67caf042352
Valerian Pittet <valerian.pittet@epfl.ch>
2014-12-18 15:08:45 +0100

    added case to result

--- MusicInterface/src/main/scala/generation/Result.scala
+++ MusicInterface/src/main/scala/generation/Result.scala
@@ -2,6 +2,7 @@ package generation
 
 trait Result[A]
 
-case class Success[A](tree: ParsingTree[A]) extends Result[A]
+case class Closed[A](tree: ClosedTree[A]) extends Result[A]
+case class Success[A](tree: OpenTree[A]) extends Result[A]
 case class Pending[A](trees: List[ParsingTree[A]]) extends Result[A]
 case object Failure extends Result[Nothing]
