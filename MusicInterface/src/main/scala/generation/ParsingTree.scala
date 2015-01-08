413407f31dce97c4d957468288faa8133b3b6c97
Valerian Pittet <valerian.pittet@epfl.ch>
2015-01-08 17:37:45 +0100

    cleaned ParsingTree.scala

--- MusicInterface/src/main/scala/generation/ParsingTree.scala
+++ MusicInterface/src/main/scala/generation/ParsingTree.scala
@@ -52,23 +52,6 @@ case class ParsingTree[A](
 
 
 
-
-  /*
-   * Closed trees accept everything, closed trees are accepted everywhere
-   * Open trees accpet only open trees with same lastWord optional
-   * Open trees with no lastword accept open trees with no lastword.
-   */
-  def accept(that: ParsingTree[A]): Option[ParsingTree[A]] =
-    // closed (main) Tree will not care anymore about refinements
-    // approach here: main grammar may finish even if secondary not
-    if (self.isClosed ||
-      that.isClosed ||
-      (self.lastWord == that.lastWord)) Some(that)
-    else None
-
-  def accept(thats: List[ParsingTree[A]]): List[ParsingTree[A]] =
-    thats flatMap { self accept _ }
-
   /** returns a list of parsing trees such that each one is
     * one word bigger and in a state where there is one single deterministic
     * step to generate a new word or closed.
@@ -78,36 +61,9 @@ case class ParsingTree[A](
     */
   def nexts(wishWords: Set[A], closable: Boolean): List[ParsingTree[A]] = {
 
-    def rejectTree(t: ParsingTree[A]): Boolean = {
-      (wishWords intersect t.nextWords).isEmpty &&
-      !(t.nullable && closable)
-    }
-
-    // accept tree if ready to generate word of wishWords
-    def acceptTree(t: ParsingTree[A]): Boolean = t.stack match {
-      case Nil => closable
-      case Word(w) :: tail =>  wishWords contains w
-      case _ => false
-    }
-
-    def acceptChild(e: GrammarElement[A], t: ParsingTree[A]): Boolean = {
-      (t.nextWordsWith(e) intersect wishWords).nonEmpty ||
-      (e.nullable && t.nullable && closable)
-    }
-
-    // generator generates only acceptable Trees
-    def generator(t: ParsingTree[A]): List[ParsingTree[A]] = {
-      t.gen(rejectTree, acceptTree, acceptChild)
-    }
-
-
-    val mainNexts: List[ParsingTree[A]] = rNexts(
-      self.genNextWord(wishWords),
-      acceptTree(_),
-      generator(_)
-    )
-
-    // refinement grammar preparation
+    // prepareNext
+    // genNextWord
+    // prepareNext
     ???
   }
 
@@ -146,20 +102,23 @@ case class ParsingTree[A](
     */
   def prepareGen(wishWords: Set[A], closable: Boolean): List[ParsingTree[A]] = {
 
+    val words = wishWords intersect nextWords
+
+    
     def rejectTree(t: ParsingTree[A]): Boolean = {
-      (wishWords intersect t.nextWords).isEmpty &&
+      (words intersect t.nextWords).isEmpty &&
       !(t.nullable && closable)
     }
 
     // accept tree if ready to generate word of wishWords
     def acceptTree(t: ParsingTree[A]): Boolean = t.stack match {
       case Nil => closable
-      case Word(w) :: tail =>  wishWords contains w
+      case Word(w) :: tail =>  words contains w
       case _ => false
     }
 
     def acceptChild(e: GrammarElement[A], t: ParsingTree[A]): Boolean = {
-      (t.nextWordsWith(e) intersect wishWords).nonEmpty ||
+      (t.nextWordsWith(e) intersect words).nonEmpty ||
       (e.nullable && t.nullable && closable)
     }
 
@@ -180,34 +139,12 @@ case class ParsingTree[A](
     // bug : if no refinements !
     // ??? : if refinement closed ?
     // todo...
-    // not that probably prepareGen will replace nexts 
-    mainReady flatMap { m => 
-      m.selectRefinements(wishWords) map { newRefs =>
-        m.updated(refs = newRefs)
-      }
-    }
+    // not that probably prepareGen will replace nexts
 
+    // TODO continue here, should be ok, need check
+    elect(mainReady flatMap (_.prepareRefs(words)))(_.probWithRefHead)
 
   }
-
-  /** applies prepareGen on refinements, then choose up to limit
-    * tuples of new refinements (to be assigned to this tree)
-    * same tuple may be selected multiple times. too hard to
-    * select each possibility only once and not bad solution
-    */
-  def selectRefinements(wishWords: Set[A]): List[List[ParsingTree[A]]] = {
-    val words = self.nextWords intersect wishWords
-    val candidates: List[List[ParsingTree[A]]] =
-      refs map { r => r.prepareGen(words, true) }
-    // a Nil value in candidates indicates that a refinement was not able to gen
-    // or that there is no refs
-    if (candidates exists { _.isEmpty }) Nil else {
-      def oneCandidate = candidates map (chooseOne(_){ _.prob })
-      List.fill(maxMemory)(oneCandidate)
-    }
-  }
-
-
 
 
   /** generates next step of search tree : List composed of parsing trees
@@ -273,120 +210,7 @@ case class ParsingTree[A](
       }
     }
 
-  /* closed indicates : no terminal generated, tree closed
-   * Success indicates : one terminal generated
-   * Pending indicates : The choices of list need more steps to decide
-   * Failure indicates : the constraints cannot be satisfied
-   */
-  private def oneGrammStep(
-    accept: GrammarElement[A] => Boolean
-  ): List[ParsingTree[A]] = stack match {
-    case Nil => List(self)
-
-    case Refine(g) :: stk =>
-      // Indicates failure of applying this refinement
-      if (refs.size >= maxRefinements) Nil
-      else self.updated(stack = stk, refs = ParsingTree(g)::refs).oneGrammStep(accept)
-
-    case (m: Message[A, _]) :: stk  =>
-      self.updated(stack = stk, msgs = m::msgs).oneGrammStep(accept)
-
-    case (t: Terminal[A]) :: stk =>
-      List(self.updated(rTree = t::rTree, stack = stk))
-
-    case (r @ Rule(body)) :: stk =>
-      List(self.updated(rTree = r :: rTree, stack = body ::: stk))
-
-    case Production(rules) :: stk => {
-      normalize(rules) { _._2 } { (x, y) => (x._1, y) } map { r =>
-        val (rule @ Rule(body), p) = r
-        self.updated(rTree = rule :: rTree, stack = body ::: stk, prob = prob*p)
-      }
-    }
-  }
-
-  /* new approach : this is the key point. At each generation step,
-   * the tree, additionnaly to the word generation will continue following
-   * rules as far as possible, while no new word is generated and the
-   * tree is still open
-   * To ease the computation, don't forget to filter branches using firsts 
-   */
-  private def prepareNextWord(wishWords: Set[A]): List[ParsingTree[A]] = {
-    stack match {
-      case Nil => List(self) // closed
-      case Word(w) :: stk => List(self) // good state, do not generate
-      case _ => ??? //oneStackStep flatMap
-    }
-  }
-
-  /* new approach : next word will return the list of trees that exactly
-   * generated one word and did not generate further.
-   * this will be useful during the first generation (btw, prepareNextWord
-   * could be used when creating a tree), but it has to return a list because
-   * refinement trees may have multiple solution to generate the one word of
-   * the main tree
-   * 
-   * new approach : generate one word and get ready for next generation
-   * generates as far as possible without another word
-   * 
-   * should return a list of possible nexts of bounded size
-   * 
-   */
-  /*
-   * contract: next will either return open trees t' with :
-   *   t.wordSize + 1 = t'.wordSize
-   * or closed trees t'' with:
-   *   t.wordSize = t'.wordSize + 1
-   * 
-   * Additionnaly, the size of returned list is limited to avoid complexity
-
-  def rNexts(solutions: List[Result[A]]): List[ParsingTree[A]] = {
-
-    self :: Nil
-  }
-   */
-  private def nextsMain: List[ParsingTree[A]] = ???/*self.oneGrammStep match {
-    case Failure => Nil
-    case Closed(tree) => List(tree)
-    case Success(tree) => List(tree)
-    case Pending(trees) => ??? /*limit(trees) flatMap _.oneGrammStep*/
-  }*/
-
-/*
-  private def nextsMain_old: List[ParsingTree[A]] = {
-    // limit with prob and not normalize here v
-    val list = self.oneGrammStep
-    val pending: List[OpenTree[A]] = list collect {
-      case that: OpenTree[A] if that.wordSize + 1 == self.wordSize => that
-    }
-    val generated: List[ParsingTree[A]] = list filterNot { pending contains _ }
-
-    // and add loop detection : compare next gen rule and probabilities
-    generated ::: (pending flatMap { _.nextsMain })
-
-  }
- */
-
-  /** Returns all ParsingTrees with head of refinements evaluated,
-    * accepted (else generated terminal equals last word of this or
-    * refinements finishes) and put at the end of the refs list.
-    * generated messages are lifted up to refined tree
-    * 
-  private def refineHead: List[ParsingTree[A]] = refs match {
-    case Nil => List(self)
-    case h::t => normalize(self accept h.nexts) map {
-      case ClosedTree(rT, p, m) =>
-        self.updated(prob = prob * p, refs = t, msgs = m ::: msgs)
-      case oT @ OpenTree(rT, s, p, r, m) =>
-        self.updated(
-          prob = prob * p,
-          refs = t :+ oT.updated(msgs = Nil),
-          msgs = oT.msgs ::: msgs)
-    }
-  }
-    */
-
-  /** variant of refineHead: applies a generate method to head of refinements and
+  /** applies a generate method to head of refinements and
     * return self.updated with each new refinement at tail of refs list
     * (for the sake of cyclic use of genHeadRef). If generated grammar is
     * closed, then do not reuse that gramar
@@ -429,10 +253,7 @@ case class ParsingTree[A](
     val generators: List[ParsingTree[A] => List[ParsingTree[A]]] =
       List.fill(refs.size)(_.prepareHeadRef(words))
 
-    def probExtractor(t: ParsingTree[A]): Double = t.refs match {
-      case Nil => t.prob
-      case r :: rs => t.prob * r.prob
-    }
+    def probExtractor(t: ParsingTree[A]): Double = t.probWithRefHead
 
     boundMultiGen(List(self), generators, probExtractor(_))
   }
@@ -450,6 +271,11 @@ case class ParsingTree[A](
   private lazy val topStkIsProd: Boolean = stack match {
     case Production(_) :: tail => true
     case _ => false
+  }
+
+  private lazy val probWithRefHead: Double = refs match {
+    case Nil => self.prob
+    case r :: rs => self.prob * r.prob
   }
 
 /*
@@ -517,13 +343,6 @@ object ParsingTree {
     // Error possible from here if targe == Nil
     electOne(target)(extract)._1.get
   }
-
-  /* c: candidate
-
-  def nextGen[A](c: OpenTree[A]): List[ParsingTree[A]] = {
-    c.nextsOneStep filterNot { _ == Failure })
-  }
-   */
 
   /** applies The algorithm.
     * Solutions is partitionned in valid solutions and pending solutions.
