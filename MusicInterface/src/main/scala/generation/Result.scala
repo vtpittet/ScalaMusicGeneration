68ef494ccb5370ceb45e3568fbf3631340a5b5d7
Valerian Pittet <valerian.pittet@epfl.ch>
2015-01-08 01:34:10 +0100

    removed closed tree

--- MusicInterface/src/main/scala/generation/Result.scala
+++ MusicInterface/src/main/scala/generation/Result.scala
@@ -2,7 +2,6 @@ package generation
 
 sealed trait Result[+A]
 
-case class Closed[A](tree: ClosedTree[A]) extends Result[A]
-case class Success[A](tree: OpenTree[A]) extends Result[A]
-case class Pending[A](trees: List[ParsingTree[A]]) extends Result[A]
+case class Success[A](tree: ParsingTree[A]) extends Result[A]
+case class Pending[A](tree: ParsingTree[A]) extends Result[A]
 case object Failure extends Result[Nothing]
