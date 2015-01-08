33be2046122de5430a62698902d0eab01ce8f7ee
Valerian Pittet <valerian.pittet@epfl.ch>
2015-01-08 21:32:14 +0100

    removed useless result

--- MusicInterface/src/main/scala/generation/Result.scala
+++
@@ -1,7 +0,0 @@
-package generation
-
-sealed trait Result[+A]
-
-case class Success[A](tree: ParsingTree[A]) extends Result[A]
-case class Pending[A](tree: ParsingTree[A]) extends Result[A]
-case object Failure extends Result[Nothing]
